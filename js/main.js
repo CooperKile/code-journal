7796f80a43f99fb92f44f5518080329341908308
CooperKile <Cooperk1807@gmail.com>
2021-07-14 12:49:05 -0700

    Useda loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ var $source = document.querySelector('img');
 var $submitForm = document.getElementById('journal-form');
 var $title = document.querySelector('.input-area');
 var $notes = document.querySelector('.notes');
+var $ul = document.querySelector('ul');
 
 // we will be listening for someone to input a url to the photo url
 // once we recive the photoURL we will update the image source
-// first we query the dom for the photoURL element
+// query the dom for the photoURL element
 // query the dom for an image source
 // add an event listener
 // if the photoURL value does not equal an empty string
@@ -30,7 +31,8 @@ $photo.addEventListener('input', function (event) {
 // reset the image previews src attribute
 // reset the input forms
 
-$submitForm.addEventListener('submit', function (event) {
+$submitForm.addEventListener('submit', submit);
+function submit(event) {
   event.preventDefault();
   var newObj = {
     input: $title.value,
@@ -42,4 +44,62 @@ $submitForm.addEventListener('submit', function (event) {
   data.entries.push(newObj);
   $source.setAttribute('src', './images/placeholder-image-square.jpg');
   $submitForm.reset($submitForm);
-});
+}
+
+// make a function that loads a dom tree
+function renderEntry(entry) {
+  var rowPadding = document.createElement('div');
+  rowPadding.className = 'row padding';
+  var columnHalf = document.createElement('div');
+  columnHalf.className = 'column-half';
+  rowPadding.appendChild(columnHalf);
+  var entryImg = document.createElement('div');
+  entryImg.className = 'entry-img';
+  columnHalf.appendChild(entryImg);
+  var imgSrc = document.createElement('img');
+  imgSrc.setAttribute = ('src', '$source.value');
+  entryImg.appendChild(imgSrc);
+  var entryText = document.createElement('div');
+  entryText.className = 'column-half';
+  var title = document.createElement('h2');
+  entryText.appendChild(title);
+  var notes = document.createElement('p');
+  entryText.appendChild(notes);
+  return rowPadding;
+}
+
+// use a loop to create a DOM tree for each entry in data model
+// loop throough the entries
+// append it to the page by appending the function to the ul
+// append it when DOMContentLoaded event is fired
+
+function appendRenderEntry(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $ul.appendChild(renderEntry(data.entries[i]));
+  }
+}
+window.addEventListener('DOMContentLoaded', appendRenderEntry);
+/** dom tree for entries
+<div class="row padding">
+  <div class="column-half">
+    <div class="entry-img">
+      <img src="./images/placeholder-image-square.jpg">
+    </div>
+  </div>
+  <div class="column-half">
+    <h1>Title</h1>
+    <p>Random text</p>
+  </div>
+</div>
+<div class="row padding">
+  <div class="column-half">
+    <div class="entry-img">
+      <img src="./images/placeholder-image-square.jpg">
+    </div>
+  </div>
+    <div class="column-half">
+      <h1>Title</h1>
+      <p>Random text</p>
+  </div>
+</div>
+*/
